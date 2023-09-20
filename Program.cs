2e2b490b19fe395fbb8426ca4616ce0c392bc328
Sarah <Sarah@HP-SARAHI>
2023-09-20 12:48:32 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearStructure
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static bool AreParenthesesBalanced(string expression)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char character in expression)
+            {
+                if (character == '(')
+                {
+                    stack.Push(character);
+                }
+                else if (character == ')')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '(')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter a mathematical expression to check if parentheses are balanced:");
+            string userInput = Console.ReadLine();
+
+            bool isBalanced = AreParenthesesBalanced(userInput);
+
+            if (isBalanced)
+            {
+                Console.WriteLine("The parentheses in the expression are balanced.");
+            }
+            else
+            {
+                Console.WriteLine("The parentheses in the expression are not balanced.");
+            }
+            Console.WriteLine();
+        }
+    }
+}
